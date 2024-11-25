be1f7fb8f096855e51da6890704a29df1b134b15
defranjo <joe.defranco@sheridancollege.ca>
2024-11-25 11:49:20 -0500

    My second comment

---
+++ src/main/java/com/mycompany/cardtrick/CardTrick.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cardtrick;
+
+/**
+ *
+ * @author joede
+ */
+public class CardTrick {
+
+    public static void main(String[] args) {
+        Card[] magicHand = new Card[7];
+        
+        // TODO:
+        // Fill in magicHand with 7 random cards
+        for (int i=0; i<magicHand.length; i++)
+        {
+            Card c = new Card();
+            //c.setValue(insert call to random number generator here)
+            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            magicHand[i] = c;
+        }
+        
+        // TODO:
+        //insert code to ask the user for Card value and suit, create their card.
+        // Search magicHand to see if they picked one of the cards from that hand
+        // Report if the card was found or not.
+    }
+}
