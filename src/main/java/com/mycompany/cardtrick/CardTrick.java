35fc05a08fa04cc88be1a30dec1542fa33e011ee
SpencerBar <srdb45@gmail.com>
2024-11-26 19:43:09 -0500

    it works, added student number and name in header

--- src/main/java/com/mycompany/cardtrick/CardTrick.java
+++ src/main/java/com/mycompany/cardtrick/CardTrick.java
@@ -7,7 +7,8 @@ import java.util.Scanner;
 
 
 /**
- *
+ *Spencer Barnes
+ * SID: 991728219
  * @author joede
  */
 public class CardTrick {
@@ -25,22 +26,30 @@ public class CardTrick {
             magicHand[i] = c;
         }
         
-        // TODO:
+      
+        
+//     for debugging  
+//        for (int i=0; i<magicHand.length; i++)
+//        {
+//            System.out.println("The card is " + magicHand[i].getValue()+" of "+magicHand[i].getSuit());
+//        }
+
         Scanner input = new Scanner(System.in);
          System.out.println("Guess Value");
          Integer cValue = Integer.valueOf(input.nextLine());
          System.out.println("Guess Suit");
          String cSuit = input.nextLine();
-        
-        //insert code to ask the user for Card value and suit, create their card.
+         //insert code to ask the user for Card value and suit, create their card.
         // Search magicHand to see if they picked one of the cards from that hand
         // Report if the card was found or not.
-        
         for (int i=0; i<magicHand.length; i++)
         {
-            if (magicHand[i].getValue() == cValue && magicHand[i].getSuit() == cSuit)
+//            for debugging
+//            System.out.println("The card is " + magicHand[i].getValue()+" of "+magicHand[i].getSuit());
+            if (magicHand[i].getValue() == cValue && magicHand[i].getSuit().toUpperCase().equals(cSuit.toUpperCase()))
             {
-                System.out.println("The card was found, it was card " + i);
+                System.out.println("The card was found, it was card " + (i+1));
+                break;
             }
             
         }
