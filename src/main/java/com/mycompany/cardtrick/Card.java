3a34e147c42f3d72e09eb5e533ae31f89ce4730d
gyvmc <gyvmc@10.16.5.33>
2024-11-26 20:32:19 -0500

    Hello\! Naman

--- src/main/java/com/mycompany/cardtrick/Card.java
+++ src/main/java/com/mycompany/cardtrick/Card.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.cardtrick;
-
+ 
 /**
  * A class that models playing card Objects. Cards have 
  * a value (note that Ace = 1, Jack -11, Queen =12, King = 13)
@@ -11,13 +11,17 @@ package com.mycompany.cardtrick;
  * There are 52 cards in a deck, no jokers.
  * This code is to be used in ICE1. When you create your own branch,
  * add your name as a modifier.
+ * 
+ * Modified by: Naman Naman (Student Number: 991754563)
+ * Date: 2024-11-26
  * @author joede
  */
 public class Card {
-    private String suit; //clubs, spades, diamonds, hearts
-   private int value;    //1-13
+    private String suit; // clubs, spades, diamonds, hearts
+    private int value;   // 1-13
 
-   public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
+    public static final String[] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
+
     /**
      * @return the suit
      */
@@ -45,5 +49,4 @@ public class Card {
     public void setValue(int value) {
         this.value = value;
     }
-   
 }
