190da5a9ede005593a3ca7f9afa5024e595e96de
sebbyds214 <sebyds14@gmail.com>
2024-11-26 21:29:02 -0500

    this is ice5: written by sebastion da silva

--- src/main/java/com/mycompany/cardtrick/CardTrick.java
+++ src/main/java/com/mycompany/cardtrick/CardTrick.java
@@ -8,18 +8,22 @@ package com.mycompany.cardtrick;
  *
  * @author joede
  */
+
+import java.util.Scanner;
 public class CardTrick {
 
     public static void main(String[] args) {
         Card[] magicHand = new Card[7];
-        
-        // TODO:
-        // Fill in magicHand with 7 random cards
+       
+          Scanner scanN = new Scanner(System.in);
+          Scanner scanS = new Scanner(System.in);
+          int numChoice;
+          String suitChoice;
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue((int)(Math.random()*13) + 0);
+            c.setSuit(Card.SUITS[(int)(Math.random()* 3) + 0]);
             magicHand[i] = c;
         }
         
@@ -27,5 +31,28 @@ public class CardTrick {
         //insert code to ask the user for Card value and suit, create their card.
         // Search magicHand to see if they picked one of the cards from that hand
         // Report if the card was found or not.
+        
+        Card userCard = new Card(); 
+        
+        System.out.println("Enter a choice for number");
+        numChoice = scanN.nextInt();
+        
+        System.out.println("Enter a suit");
+        suitChoice = scanS.next();
+        
+        userCard.setSuit(suitChoice);
+        userCard.setValue(numChoice);
+        
+        for (Card card: magicHand){
+            
+            if(card.equals(userCard)){
+                System.out.println("Card was found!");
+                break;
+            }
+            else{
+                System.out.println("Card not found.");
+            }
+            
+        }
     }
 }
