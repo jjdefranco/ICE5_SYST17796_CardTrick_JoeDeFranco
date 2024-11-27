48411ccda3e88de878a8ae85009dce0d70b9bfb1
yuehou <houyue@sheridancollege.ca>
2024-11-26 20:50:04 -0500

    Yue Hou Branch update

--- src/main/java/com/mycompany/cardtrick/CardTrick.java
+++ src/main/java/com/mycompany/cardtrick/CardTrick.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.cardtrick;
 
+import java.util.*; 
+import java.util.Scanner;
 /**
  *
  * @author joede
@@ -18,14 +20,50 @@ public class CardTrick {
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            int num = (int) (Math.random() * 13) + 1;  // random number 1-13 
+            c.setValue(num);
+            //System.out.println(num);
+            
+            c.setSuit(Card.SUITS[(int) (Math.random() * 4)]);  // random number 0 - 3 
             magicHand[i] = c;
+            System.out.println(magicHand[i].getSuit() + magicHand[i].getValue());
         }
+        
+       
         
         // TODO:
         //insert code to ask the user for Card value and suit, create their card.
         // Search magicHand to see if they picked one of the cards from that hand
         // Report if the card was found or not.
+        Scanner scanner = new Scanner(System.in); 
+          
+        System.out.println("Enter the suit (Hearts, Diamonds, Spades, Clubs):");
+        String userSuit = scanner.nextLine();
+        System.out.println("Enter the value (1-13):");
+        int userValue = scanner.nextInt();
+
+        Card userCard = new Card();
+        userCard.setSuit(userSuit);
+        userCard.setValue(userValue);
+
+        
+        boolean found = false;
+        for (Card c : magicHand) {
+            if (c.getSuit().equals(userCard.getSuit()) && c.getValue() == userCard.getValue()) {
+                found = true;
+                break;
+            }
+        }
+
+        // Report the result
+        if (found) {
+            System.out.println("Yes you have the card");
+        } else {
+            System.out.println("No, the card is not found. ");
+        }
+
+        scanner.close();
+        
+        
     }
 }
