e831d8ed44b9de5d5f79937ef2eb4396c07f9472
Ujjwal Koirala <koiralaujjwal321@gmail.com>
2024-11-26 20:41:28 -0500

    GitTest

--- src/main/java/com/mycompany/cardtrick/Card.java
+++ src/main/java/com/mycompany/cardtrick/Card.java
@@ -1,49 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.cardtrick;
-
-/**
- * A class that models playing card Objects. Cards have 
- * a value (note that Ace = 1, Jack -11, Queen =12, King = 13)
- * A suit (clubs, hearts, spades, diamonds).
- * There are 52 cards in a deck, no jokers.
- * This code is to be used in ICE1. When you create your own branch,
- * add your name as a modifier.
+ 
+/* 
+ * Modified by: Ujjwal Koirala (Student Number: 991738145)
+ * Date: 2024-11-26
  * @author joede
  */
 public class Card {
-    private String suit; //clubs, spades, diamonds, hearts
-   private int value;    //1-13
+    private String suit;
+    private int value;
 
-   public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
-    /**
-     * @return the suit
-     */
+    public static final String[] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
+
     public String getSuit() {
         return suit;
     }
 
-    /**
-     * @param suit the suit to set
-     */
     public void setSuit(String suit) {
         this.suit = suit;
     }
 
-    /**
-     * @return the value
-     */
     public int getValue() {
         return value;
     }
 
-    /**
-     * @param value the value to set
-     */
     public void setValue(int value) {
         this.value = value;
     }
-   
 }
