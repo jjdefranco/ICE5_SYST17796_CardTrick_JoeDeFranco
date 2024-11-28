739d4ed5fa6d5646ef7990a43d93a93e3ce4d65d
Steven Zhao <stezha496@gmail.com>
2024-11-27 20:48:31 -0500

    Branch code changes

--- src/main/java/com/mycompany/cardtrick/CardTrick.java
+++ src/main/java/com/mycompany/cardtrick/CardTrick.java
@@ -4,28 +4,54 @@
 
 package com.mycompany.cardtrick;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author joede
+ * @contributor Steven Zhao
  */
 public class CardTrick {
 
     public static void main(String[] args) {
         Card[] magicHand = new Card[7];
+        Random randValue = new Random();
+        boolean cardThere = false;
         
-        // TODO:
-        // Fill in magicHand with 7 random cards
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue(randValue.nextInt(1,13));
+            c.setSuit(Card.SUITS[randValue.nextInt(3)]);
             magicHand[i] = c;
+            //System.out.println(c.getSuit()+ " --- " + c.getValue());
         }
         
-        // TODO:
+        Scanner userIn = new Scanner(System.in);
+
         //insert code to ask the user for Card value and suit, create their card.
+        System.out.println("Enter a card suit: ");
+        String userSuit = userIn.nextLine();
+        
+        System.out.println("Enter a card value : ");
+        int userValue = userIn.nextInt();
+       
+        //System.out.println(userValue + " --- " + userSuit);
         // Search magicHand to see if they picked one of the cards from that hand
+        for(int i=0; i<magicHand.length; i++){
+            if((magicHand[i].getSuit().compareToIgnoreCase(userSuit) == 0) && (magicHand[i].getValue() == userValue)){
+                cardThere = true;
+                break;
+            }
+        }
+        
         // Report if the card was found or not.
+        if(cardThere){
+            System.out.println("Card is in the hand");
+        }
+        else{
+            System.out.println("Card is not in the hand");
+        }
     }
 }
