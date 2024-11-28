8735e3e8ead4cb18bd067f424fafd9d114176ba9
CodeAwais <shahidaw@sheridancollege.ca>
2024-11-27 20:59:36 -0500

    ICE 5 for Awais Shahid

--- src/main/java/com/mycompany/cardtrick/CardTrick.java
+++ src/main/java/com/mycompany/cardtrick/CardTrick.java
@@ -3,6 +3,8 @@
  */
 
 package com.mycompany.cardtrick;
+import java.util.Random;
+import java.util.Scanner;
 
 /**
  *
@@ -13,13 +15,21 @@ public class CardTrick {
     public static void main(String[] args) {
         Card[] magicHand = new Card[7];
         
+        Random randNumGenerator = new Random();
+        Scanner input  = new Scanner(System.in);
+        
         // TODO:
         // Fill in magicHand with 7 random cards
         for (int i=0; i<magicHand.length; i++)
         {
+            int randNumCard = (randNumGenerator.nextInt(13)) + 1;
+            int randNumSuit = (randNumGenerator.nextInt(4));
+            
             Card c = new Card();
             //c.setValue(insert call to random number generator here)
+            c.setValue(randNumCard);
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setSuit(Card.SUITS[randNumSuit]);
             magicHand[i] = c;
         }
         
@@ -27,5 +37,38 @@ public class CardTrick {
         //insert code to ask the user for Card value and suit, create their card.
         // Search magicHand to see if they picked one of the cards from that hand
         // Report if the card was found or not.
+        
+        System.out.println("Enter a Card Value from 1-13: ");
+        int card  = input.nextInt();
+        input.nextLine();
+        
+        System.out.println("Enter a Suit Rank from 0-3: ");
+        String suitRank = input.nextLine();
+        
+        Card userCard = new Card();
+        userCard.setValue(card);
+        userCard.setSuit(suitRank);
+        
+        boolean cardIsFound = false;
+        
+        for (Card c : magicHand) {
+        
+            if (c.getValue() == userCard.getValue() && c.getSuit().equalsIgnoreCase(userCard.getSuit())) {
+                cardIsFound = true;
+                break;
+            }
+        }
+        
+         // Now we will check if the cards matched or not after completing the loop
+
+        if (cardIsFound == true){
+            System.out.println("Card Matches!!!");
+        }
+        
+        else {
+            System.out.println("Card does not Match");
+        }
+        
+        
     }
 }
