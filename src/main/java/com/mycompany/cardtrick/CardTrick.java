ae422946a26e6fa118a0c1248005b32e488f7119
Dru Aron <dru_aron_caparro@gmail.com>
2024-11-27 20:54:51 -0500

    MY FIRST COMMIT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1111

--- src/main/java/com/mycompany/cardtrick/CardTrick.java
+++ src/main/java/com/mycompany/cardtrick/CardTrick.java
@@ -3,29 +3,57 @@
  */
 
 package com.mycompany.cardtrick;
+import java.lang.Math;
+import java.util.Scanner;
 
 /**
  *
  * @author joede
+ * @author DrooAC
  */
 public class CardTrick {
 
     public static void main(String[] args) {
         Card[] magicHand = new Card[7];
+        Scanner input = new Scanner(System.in);
+        Card userCard = new Card();
         
-        // TODO:
-        // Fill in magicHand with 7 random cards
-        for (int i=0; i<magicHand.length; i++)
-        {
+        boolean found = false;
+        int cardValue = 0;
+        int cardSuit = 0;
+        
+        for (int i = 0; i < magicHand.length; i++) {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue((int) (Math.random() * 13 + 1));
+            c.setSuit(Card.SUITS[(int) (Math.random() * 4)]);
             magicHand[i] = c;
         }
         
-        // TODO:
-        //insert code to ask the user for Card value and suit, create their card.
-        // Search magicHand to see if they picked one of the cards from that hand
-        // Report if the card was found or not.
+        System.out.println("Enter a Card Value (numeric 1-13 please I have no error checking)!!");
+        cardValue = input.nextInt();
+        System.out.println("Enter a Card Suit (numeric 0-3 please I have no error checking)!!");
+        cardSuit = input.nextInt();
+        
+        userCard.setValue(cardValue);
+        userCard.setSuit(Card.SUITS[cardSuit]);
+        
+        System.out.printf("Your Card: %s of %s.\n", userCard.getValue(), userCard.getSuit());
+        System.out.println();
+        
+        for (int i = 0; i < magicHand.length; i++) {
+            System.out.printf("Card %d: %s of %s.\n", i + 1, magicHand[i].getValue(), magicHand[i].getSuit());
+            System.out.println();
+            if (userCard.getValue() == magicHand[i].getValue() && 
+                userCard.getSuit().equals(magicHand[i].getSuit())) {
+                found = true;
+                break;
+            }
+        }
+        
+        if (found) {
+            System.out.println("Congratulation! You found a matching card!");
+        } else {
+            System.out.println("Not congratulation! You did not find a matching card!");
+        }
     }
 }
